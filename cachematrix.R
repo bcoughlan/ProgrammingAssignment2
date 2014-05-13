e5939f9dfe5f6ad0bd7912663173699d660ca47e
Barry Coughlan <barry@091labs.com>
2014-05-13 22:33:52 +0200

    Assignment solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for caching the inverse of a matrix
 
-## Write a short comment describing this function
-
+## Encapsulates a supplied matrix to enable caching of matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		inverse <- NULL
+	}
+	get <- function() x
+	setInverse <- function(inv) inverse <<- inv
+	getInverse <- function() inverse
 
+	list(set=set, get=get, setInverse=setInverse,
+		    getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Compute matrix inverse. Return cached inverse
+## if previously computed for this matrix, otherwise
+## sets the cache value for future use.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setInverse(m)
+        m
 }
+
+## Test code. First computation should not print "getting cached data".
+## Other two computations should print "getting cached data".
+
+#m <- matrix(1:4, 2, 2)
+#c <- makeCacheMatrix(m)
+#message("Running first")
+#print(cacheSolve(c))
+#message("Running second")
+#print(cacheSolve(c))
+#message("Running third")
+#print(cacheSolve(c))
